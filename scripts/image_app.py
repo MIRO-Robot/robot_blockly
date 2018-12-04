d20907ce28e88f59ab40086a8f62685143344195
Daniel <dcam0050>
2018-12-04 00:59:02 +0000

    Integrated image visualisation

--- scripts/image_app.py
+++ scripts/image_app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, Response
 import rospy
 from sensor_msgs.msg import Range, Image, CompressedImage
+from std_msgs.msg import String
 
 import numpy as np
 import cv2
@@ -11,9 +12,8 @@ import sys
 import os
 
 rospy.init_node('image_server', anonymous=True, disable_signals=True)
-rate = rospy.Rate(10)
+rate = rospy.Rate(100)
 app = Flask(__name__)
-current_robot = "sim01"
 
 @app.route('/')
 def index():
@@ -21,23 +21,50 @@ def index():
     return render_template('index.html')
 
 
-def gen():
+def gen(stream_name=""):
     """Video streaming generator function."""
     try:
         while True:
-            image_l = rospy.wait_for_message('/miro/' + current_robot + '/platform/caml/compressed', CompressedImage, timeout=10)
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + image_l.data + b'\r\n')
+            try:
+                if stream_name == "right":
+                    topic = '/miro/sim01/platform/camr/compressed'
+                elif stream_name == "left":
+                    topic = '/miro/sim01/platform/caml/compressed'
+                elif stream_name == "threshold":
+                    topic = "/blockly/imageVariables/threshold/compressed"
+                
+                image_data = rospy.wait_for_message(topic, CompressedImage, timeout=10)
+                
+                if image_data is not None:
+                #     image_name = rospy.wait_for_message('/blocklyVariables/image_name', String, timeout=1)
+                #     if image_name is not None:
+                #         print(image_name)
+                    yield (b'--frame\r\n'
+                           b'Content-Type: image/jpeg\r\n\r\n' + image_data.data + b'\r\n')
+            except rospy.exceptions.ROSException as e:
+                pass
     except Exception as e:
         print(e)
     except KeyboardInterrupt:
         rospy.signal_shutdown("interrupt")
 
 
-@app.route('/video_feed')
-def video_feed():
+@app.route('/miro_left')
+def miro_left():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
+    return Response(gen("left"),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/miro_right')
+def miro_right():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen("right"),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/colour_thresh')
+def colour_thresh():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen("threshold"),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
